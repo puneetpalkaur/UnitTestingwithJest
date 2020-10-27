de8e22166256dddf4f32700322d5cdf3f1cd5960
puneetpalkaur <billingpuneet.13@gmail.com>
2020-10-26 22:23:53 -0400

    Unit Tests NodeJs + Jest

---
+++ Unit Tests/lib.test.js
@@ -0,0 +1,37 @@
+const lib = require('./lib');
+
+describe('absolute',() =>{
+    it('Always return positive number if input is positive',() =>{
+        let result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
+    it('Always return positive number if input is negative',() =>{
+        let result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
+});
+
+
+describe('greet',() => {
+    it('should return greeting function',() =>{
+        let result = lib.greet('Welcome Puneet');
+        expect(result).toMatch(/Puneet/); // or use toContain
+    });
+});
+
+describe('getCurrencies',() => {
+    it('should return supporting currencies',() =>{
+        let result = lib.getCurrencies();
+        // too general expect(result).toBeDefined();
+        // too specific expect(result[0]).toBe('USD');
+       // expect(result).toContain('USD');
+        expect(result).toEqual(expect.arrayContaining(['USD','AUD','EUR']));
+    });
+});
+
+describe('getProduct',() => {
+    it('should return product with given id ',() =>{
+        let result = lib.getProduct(1);
+        expect(result).toEqual({id: 1, price:10})
+    });
+});
